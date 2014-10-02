25d29d1cda7f3ad55b52005c823294532f35694e
John Tran <johnjtran@icloud.com>
2014-10-02 04:29:46 -0700

    update lab 1 template

---
+++ src/lab1/RSquare.java
@@ -0,0 +1,19 @@
+package lab1;
+
+public class RSquare {
+	
+	public static void rsquare(int level, double w, double x, double y) {
+		if (level == 0) {
+			// maybe draw something before you return?!?!
+			return;
+		} else {
+			// compute grey
+			// compute xp, yp
+			// call rsquare ( ... )
+		}
+	}
+	
+	public static void main(String[] args) {
+		rsquare(5, 1.0, 0.0, 1.0);		
+    }
+}
--- src/library/StdDraw.java
+++ src/library/StdDraw.java
@@ -1,3 +1,4 @@
+package library;
 /*************************************************************************
  *  Compilation:  javac StdDraw.java
  *  Execution:    java StdDraw
